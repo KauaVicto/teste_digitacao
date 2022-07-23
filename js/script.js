c1a390e541321b2a46080c3b28a4ec2f72866eee
Kauã Victor <kauavictor259@gmail.com>
2022-07-23 01:06:15 -0300

    Finalizado projeto de teste de digitação

--- js/script.js
+++ js/script.js
@@ -2,31 +2,53 @@ $(document).ready(function () {
     $('#campotexto').focus(); // mantem o foco no input
 
     var texto = $('#texto').text().trim(); // texto inteiro
+    var letrasSeparadas = TrocarTexto(texto);
 
-    var letrasSeparadas = texto.split(''); // separa todas as letras em um array
+    /* Texto próprio */
 
-    /* Separado cada caracter uma tag span */
-    var novoTexto = '';
-    for (var i = 0; i < letrasSeparadas.length; i++) {
-        if (i == 0) {
-            novoTexto += '<span class="proxima">' + letrasSeparadas[i] + '</span>';
+    $('#inserir-texto-proprio').on('click', function (e) {
+
+        $('.texto-proprio').show('200');
+        $('.jogo-home').hide('200');
+        $(this).hide('200');
+
+    })
+
+
+    $('#env-texto-usuario').on('click', function (e) {
+        texto = $('#textarea-texto-proprio').val();
+        if (texto.length < 20) {
+            alert('É necessário um texto com mais que 20 caracteres. Falta ' + (20 - texto.length));
         } else {
-            novoTexto += '<span class="normal">' + letrasSeparadas[i] + '</span>';
-        }
-    }
-    $('#texto').html(novoTexto);
+            letrasSeparadas = TrocarTexto(texto);
 
-    /* Caso clique em qualquer área da página irá focar no input */
-    $(document).on('click', function () {
-        $('#campotexto').focus();
+            $('.texto-proprio').hide('200');
+            $('.jogo-home').show('200');
+            $('#inserir-texto-proprio').show('200');
+
+            $('#textarea-texto-proprio').val('')
+        }
+    });
+    $('#cancelar-texto-usuario').on('click', function (e) {
+        $('.texto-proprio').hide('200');
+        $('.jogo-home').show('200');
+        $('#inserir-texto-proprio').show('200');
     });
 
-    var ctPalavra = 0; // quantidade de palavras digitadas
+
+    /* Caso clique em qualquer área da página irá focar no input */
+    $('body').on('click', function (e) {
+        if (e.target.id != 'textarea-texto-proprio') {
+            $('#campotexto').focus();
+        }
+    });
+
+    var ctPalavras = 0; // quantidade de palavras digitadas
     var ctLetras = 0; // quantidade de letras digitadas
+    var ctAcertos = 0; // quantidade de acertos
     var ctErros = 0; // quantidade de erros cometidos
     $('#campotexto').on('input', function (e) {
-
-        var letraDigitada = $(this).val().split('');
+        var letraDigitada = $(this).val();
 
         var letraCerta = $('.proxima').text();
 
@@ -37,31 +59,49 @@ $(document).ready(function () {
             $('.normal:first').addClass('proxima') // adiciona a classe 'proxima' na próxima letra
             $('.proxima').removeClass('normal') // remove a classe 'normal' na próxima letra
 
-            $('.certo').removeClass('errado') // remove a classe 'errado' em qualquer letra com classe 'certo'
-
-            ctLetras++;
+            $('.certo').removeClass('errado')
+            ctAcertos++;
         } else {
-            $('.proxima').addClass('errado') // adiciona a classe 'errado' na letra quando digitou errado
+            $('.proxima').addClass('errado')
             ctErros++;
         }
+        ctLetras++;
 
         // quando é digitado espaço soma mais uma palavra
         if (e.originalEvent.data == ' ') {
-            ctPalavra++;
+            ctPalavras++;
         }
 
         /* Quando todas as letras forem digitadas certo vai para o resultado final */
-        if (ctLetras >= letrasSeparadas.length) {
+        if (ctAcertos >= letrasSeparadas.length) {
             $('.digitacao').hide(200)
             $('.resultado').show(200)
 
-            /* Calcula a média de todos os tempos */
+            /* Calcula a média de todos os tempos das letras */
             let soma = 0;
-            for (let i = 0; i < tempos.length; i++) {
-                soma += tempos[i];
+            for (let i = 0; i < temposLetra.length; i++) {
+                soma += temposLetra[i];
             }
-            let ppmfinal = soma / tempos.length;
-            $('#ppm-final').text(ppmfinal.toFixed(1))
+            let lpmfinal = soma / temposLetra.length;
+            lpmfinal = (!isNaN(lpmfinal)) ? lpmfinal : 0;
+
+            $('#lpm-final').text(lpmfinal.toFixed(1));
+
+            /* Calcula a média de todos os tempos das palavras */
+            soma = 0;
+            for (let i = 0; i < temposPalavra.length; i++) {
+                soma += temposPalavra[i];
+            }
+            let ppmfinal = soma / temposPalavra.length;
+
+            ppmfinal = (!isNaN(ppmfinal)) ? ppmfinal : 0;
+            $('#ppm-final').text(ppmfinal.toFixed(1));
+
+
+
+            /* Calcula o percentual de acertos */
+            let percentAcertos = (ctAcertos * 100) / ctLetras;
+            $('#percent-acertos').text(percentAcertos.toFixed(1) + '%')
 
         }
 
@@ -72,33 +112,75 @@ $(document).ready(function () {
     /* Conta os segundos após começar a digitar */
     var segundos = 0;
     setInterval(function () {
-        if (ctPalavra >= 1) {
+        if (ctLetras >= 1) {
             segundos += 0.1;
         }
     }, 100)
 
+
+    /* Calcula a quantidade de letras por minuto */
+    var temposLetra = [];
     /* Calcula a quantidade de palavras por minuto */
-    var tempos = [];
+    var temposPalavra = [];
     setInterval(function () {
-        if (ctPalavra >= 1) {
-            var ppm = (ctPalavra / segundos) * 60;
-            tempos.push(ppm);
+        /* letras por minuto */
+        if (ctLetras >= 1) {
+            var lpm = (ctLetras / segundos) * 60;
+            temposLetra.push(lpm);
+            $('#lpm').text(lpm.toFixed(1))
+        }
+
+        /* palavras por minuto */
+        if (ctPalavras >= 1) {
+            var ppm = (ctPalavras / segundos) * 60;
+            temposPalavra.push(ppm);
             $('#ppm').text(ppm.toFixed(1))
         }
     }, 1000);
 
 
-
     $('#recomecar').on('click', function () {
+        reiniciar();
+    })
+
+
+
+    function reiniciar() {
         $('.digitacao').show(200)
         $('.resultado').hide(200)
+        $('#ppm').text(0.0)
+        $('#lpm').text(0.0)
         ctLetras = 0;
-        ctPalavra = 0;
+        ctPalavras = 0;
         segundos = 0;
         ctErros = 0;
-        tempos = [];
+        ctAcertos = 0;
+        temposLetra = [];
+        temposPalavra = [];
 
         $('#texto span').removeClass('certo errado').addClass('normal')
         $('.normal:first').addClass('proxima').removeClass('normal')
-    })
+    }
+
+    function TrocarTexto(texto) {
+        var letrasSeparadas = texto.split(''); // separa todas as letras em um array
+
+        /* Separa cada caracter uma tag span */
+        var novoTexto = '';
+        for (var i = 0; i < letrasSeparadas.length; i++) {
+            if (i == 0) {
+                novoTexto += '<span class="proxima">' + letrasSeparadas[i] + '</span>';
+            } else {
+                novoTexto += '<span class="normal">' + letrasSeparadas[i] + '</span>';
+            }
+        }
+        $('#texto').html(novoTexto);
+
+        reiniciar();
+
+        return letrasSeparadas;
+    }
 });
+
+
+
